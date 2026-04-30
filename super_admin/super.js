07f2a5654b11515bbeb00e08edce8e0b4fe2edc7
Kaisalafef <142940796+Kaisalafef@users.noreply.github.com>
2026-04-30 10:59:40 +0300

    W5

--- super_admin/super.js
+++ super_admin/super.js
@@ -26,18 +26,8 @@ async function checkAuth() {
       return null;
     }
 
-<<<<<<< HEAD
-    try {
-        const res = await fetch('https://flashpay-back-1.onrender.com/api/me', {
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Accept': 'application/json',
-            }
-        });
-=======
     const data = await res.json();
     const userRole = data.user.role;
->>>>>>> e0f0708326dc814bcf3ab8258724fe0d0b55a4a3
 
     const ALLOWED_ROLES = ["super_admin"];
 
